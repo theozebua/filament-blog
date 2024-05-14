12e0296903f6b3393036583d3d585f9038ebae0b
Theo Zebua <ronizebua.rz@gmail.com>
2024-05-14 16:09:31 +0700

    `chore:` format with pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,9 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     protected $hidden = [
         'password',
