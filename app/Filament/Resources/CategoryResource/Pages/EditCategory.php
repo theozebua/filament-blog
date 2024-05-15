3bb7feda3d43c2c84510f1ca54a58893835ddedc
Theo Zebua <ronizebua.rz@gmail.com>
2024-05-15 11:02:51 +0700

    `fix:` deleting category

--- app/Filament/Resources/CategoryResource/Pages/EditCategory.php
+++ app/Filament/Resources/CategoryResource/Pages/EditCategory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Filament\Resources\CategoryResource\Pages;
 
 use App\Filament\Resources\CategoryResource;
+use App\Models\Category;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
@@ -15,7 +16,11 @@ class EditCategory extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+                ->before(function (Category $record): void {
+                    // Delete relations to posts before deleting the category
+                    $record->posts()->detach();
+                }),
         ];
     }
 }
