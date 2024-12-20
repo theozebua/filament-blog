3b2a517863323d9424c79fdb44c297d2353ddf86
Theo <ronizebua.rz@gmail.com>
2024-12-20 16:05:34 +0700

    `chore:` add default cover and categories

--- tests/Feature/Filament/Resources/BasePostResource.php
+++ tests/Feature/Filament/Resources/BasePostResource.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Filament\Resources;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\SUpport\Str;
 use Tests\Feature\Filament\FilamentBaseTestCase;
 
 class BasePostResource extends FilamentBaseTestCase
@@ -20,5 +23,10 @@ class BasePostResource extends FilamentBaseTestCase
         parent::setUp();
 
         $this->posts = Post::factory(10)->create(['user_id' => $this->user->getKey()]);
+
+        $this->posts->each(function (Post $post): void {
+            $post->addMedia(UploadedFile::fake()->image(Str::random() . '.jpg'))->toMediaCollection('covers');
+            $post->categories()->attach(Category::factory(4)->create()->pluck('id'));
+        });
     }
 }
